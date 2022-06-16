d8f2a593f51caa710492e6cbf8a53dddf6f57556
lancewrites <lancewrites77@gmail.com>
2022-06-16 10:55:07 -0700

    challenge done, validations done in rspec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a title' do
+    my_blog = Blog.create title: nil, content: 'blog blog blog blog blog'
+    expect(my_blog.errors[:title]).to_not be_empty
+  end
+  it 'is not valid without content' do
+    my_blog = Blog.create title: 'This is my blog', content: nil
+    expect(my_blog.errors[:content]).to_not be_empty
+  end
+  it 'has 10 or more characters in the title' do
+    my_blog = Blog.create title:'123456789', content: 'blog blog blog blog blog'
+    expect(my_blog.errors[:title]).to_not be_empty
+  end
+  it 'has unique titles' do
+    Blog.create title:'THIS HAS AT LEAST TEN', content: 'blog blog blog blog blog'
+    my_blog = Blog.create title:'THIS HAS AT LEAST TEN', content: 'blog blog blog blog blog'
+    expect(my_blog.errors[:title]).to_not be_empty
+  end
 end
